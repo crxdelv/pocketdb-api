3b94e54e74f09af60b3d0045b26c3c4f0eae7996
creuser <151720755+creuserr@users.noreply.github.com>
2024-02-04 23:11:12 +0800

    update get.js

--- api/get.js
+++ api/get.js
@@ -8,3 +8,20 @@ const ERR_PARAM = [
   "INCOMPLETE_PARAM",
   "Incomplete parameters. Please view the documentation https://pocketdb-api.vercel.app/docs/get for more information.",
 ];
+
+module.exports = async (req, res) => {
+  function resolve(data, reqdata) {
+    res.status(200).send(JSON.stringify({
+      success: true,
+      result: data,
+      request: reqdata
+    }));
+  }
+  function reject([ error, message ], reqdata) {
+    res.status(status).send(JSON.stringify({
+      success: false,
+      error, message,
+      request: reqdata
+    }));
+  }
+}
