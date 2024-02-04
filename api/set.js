b8be402724020f4aee558a49b8ff1870cfe1bd11
creuser <151720755+creuserr@users.noreply.github.com>
2024-02-04 22:51:26 +0800

    update set.js

--- api/set.js
+++ api/set.js
@@ -1,6 +1,19 @@
 const pocketdb = require('./pocketdb.js');
 module.exports = async (req, res) => {
   function resolve(data, reqdata) {
+    res.status(status).send(JSON.stringify({
+      success: true,
+      result: data,
+        request: reqdata
+    }));
+  }
+  function reject(status, error, message, reqdata) {
+    res.status(status).send(JSON.stringify({
+       success: true,
+        result: data,
+        request: reqdata
+      }));
+  }
     if(status >= 400) {
       res.status(status).send(JSON.stringify({
         success: false,
@@ -8,11 +21,7 @@ module.exports = async (req, res) => {
         request: reqdata
       }));
     } else {
-      res.status(status).send(JSON.stringify({
-        success: true,
-        result: data,
-        request: reqdata
-      }));
+      
     }
   }
   if(
