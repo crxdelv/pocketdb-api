57aa4bcfe012e0dc0db4f8be7ac511abb2ca599e
creuser <151720755+creuserr@users.noreply.github.com>
2024-02-04 22:47:25 +0800

    update set.js

--- api/set.js
+++ api/set.js
@@ -2,7 +2,15 @@ const pocketdb = require('./pocketdb.js');
 module.exports = async (req, res) => {
   function end(status, data) {
     if(status >= 400) {
-      res.status(status).send(JSON.stringify({}));
+      res.status(status).send(JSON.stringify({
+        success: false,
+        error: data
+      }));
+    } else {
+      res.status(status).send(JSON.stringify({
+        success: true,
+        result: data
+      }));
     }
   }
   if(req.query.token != null)
